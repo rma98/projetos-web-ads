a5e95899e86fc9860580333d46c7f17f6d9e5ee7
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-09-12 16:45:13 -0300

    startup | landing-page | js | scripts.js

---
+++ startup/landing-page/js/scripts.js
@@ -0,0 +1,5 @@
+const botaoAlugarAgora = document.getElementById('alugar-agora');
+
+botaoAlugarAgora.addEventListener('click', function() {
+    window.location.href = '../tela-de-cadastro/cadastro-locatario.html';
+});
